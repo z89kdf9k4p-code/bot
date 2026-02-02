a46a419d74580d3dade4c69a86a67a6755e4c5ad
z89kdf9k4p-code <z89kdf9k4p@privaterelay.appleid.com>
2026-02-02 23:00:10 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,11 +1,14 @@
 from aiogram.fsm.state import StatesGroup, State
 
+# ===== Выбор языка =====
 class LanguageState(StatesGroup):
     lang = State()
 
+# ===== Регистрация пользователя =====
 class Register(StatesGroup):
     role = State()
     shop = State()
 
+# ===== Обратная связь =====
 class FeedbackState(StatesGroup):
     text = State()
