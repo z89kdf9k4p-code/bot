2302243695600c3a20e6da580ae38bf68a8def6c
z89kdf9k4p-code <z89kdf9k4p@privaterelay.appleid.com>
2026-02-02 22:59:48 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,20 +1,22 @@
-_users = {}
-_feedbacks = {}
+# ===== Простейшая "БД" в памяти =====
+users_db = {}
+feedback_db = []
 
 def get_user(user_id):
-    return _users.get(user_id)
+    return users_db.get(user_id)
 
-def get_all_users():
-    return list(_users.values())
-
-def save_user(user_id, username, role=None, shop=None, lang=None):
-    _users[user_id] = (user_id, username, role, shop, lang)
+def save_user(user_id, username, role=None, shop=None, lang="RU"):
+    users_db[user_id] = (user_id, username, role, shop, lang)
 
 def save_feedback(user_id, text):
-    _feedbacks[user_id] = text
+    feedback_db.append((user_id, text))
 
 def cleanup_feedback(user_id=None):
+    global feedback_db
     if user_id:
-        _feedbacks.pop(user_id, None)
+        feedback_db = [f for f in feedback_db if f[0] != user_id]
     else:
-        _feedbacks.clear()
+        feedback_db = []
+
+def get_all_users():
+    return list(users_db.values())
