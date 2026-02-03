fc7c9b00c08950ffbad08c9f7f5df6f57e56a2fc
z89kdf9k4p-code <z89kdf9k4p@privaterelay.appleid.com>
2026-02-04 00:18:22 +0300

    Add files via upload

--- states.py
+++ states.py
@@ -6,9 +6,19 @@ class LanguageState(StatesGroup):
 
 # ===== Регистрация пользователя =====
 class Register(StatesGroup):
+    phone = State()
     role = State()
     shop = State()
 
 # ===== Обратная связь =====
 class FeedbackState(StatesGroup):
+    text = State()
+
+# ===== FAQ поиск =====
+class FAQState(StatesGroup):
+    query = State()
+
+# ===== Напоминания =====
+class ReminderState(StatesGroup):
+    minutes = State()
     text = State()
